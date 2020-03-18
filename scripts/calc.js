3688a6d1a3a0e11961febf49bb3b7558413ac657
Datum <allen.james.west@gmail.com>
2020-03-18 21:17:51 +0000

    finished first section challenges

--- scripts/calc.js
+++ scripts/calc.js
@@ -3,6 +3,54 @@ function addition(numOne, numTwo) {
         return numOne + numTwo;
     }
     else {
-        return "Error!"
+        alert("Error!");
+    }
+}
+calculator = function () {
+    this.value = 0;
+};
+
+calculator.prototype.add == function(number) {
+    if (typeof(number) == "number") {
+        this.value += number;
+    }
+    else {
+        alert("Error!");
+    }
+};
+
+function whatCanIDrink (age){
+    if(age < 1){
+        return "Sorry. I can't tell what drink because that age is incorrect!";
+    }
+    if(age < 14 && age > 1){
+        return "Drink Toddy";
+    }
+    if(age < 18 && age > 14){
+        return "Drink Coke";
+    }
+    if(age < 21 && age > 18){
+        return "Drink Beer";
+    }
+    if(age < 130 && age > 21){
+        return "Drink Whiskey"
+    }
+    else{
+        return "Sorry i can't tell what drink because that age is incorrect!";
+    }
+};
+
+function fizzBuzz(number) {
+    if(number % 3 && number % 5){
+        return "fizzBuzz";
+    }
+    if(number % 3){
+        return "Fizz";
+    }
+    if(number % 5){
+        return "Buzz";
+    }
+    else{
+        return number;
     }
 }
