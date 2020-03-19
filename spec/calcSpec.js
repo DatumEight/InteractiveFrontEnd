e1431c169c2555c9c8d39ac30ec7fc9e5f45e3ba
Datum <allen.james.west@gmail.com>
2020-03-19 18:35:25 +0000

    added tests for Drink function

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -25,3 +25,20 @@ describe ("fizzBuzz", function(){
        expect(fizzBuzz(20)).toBe("buzz");
     });
 });
+describe ("whatCanIDrink", function(){
+    it("should return Sorry. I can't tell what drink because that age is incorrect!", function(){
+        expect(whatCanIDrink(0)).toBe("Sorry. I can't tell what drink because that age is incorrect!");
+    });
+       it("should return Drink Toddy", function(){
+        expect(whatCanIDrink(8)).toBe("Drink Toddy");
+    });
+       it("should return Drink Coke", function(){
+        expect(whatCanIDrink(16)).toBe("Drink Coke");
+    });
+       it("should return Drink Beer", function(){
+        expect(whatCanIDrink(20)).toBe("Drink Beer");
+    });
+       it("should return Drink Whiskey", function(){
+        expect(whatCanIDrink(25)).toBe("Drink Whiskey");
+    });
+});
